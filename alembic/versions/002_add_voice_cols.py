69283580514502f6e3d84ba2c4dac4e97be1483e
Dim41g <ivanov1331d@gmail.com>
2025-12-15 17:29:33 +0300

    Fix migration filename length issue - rename 002_add_voice_columns_for_personalization.py to 002_add_voice_cols.py

--- alembic/versions/002_add_voice_cols.py
+++ alembic/versions/002_add_voice_cols.py
@@ -1,6 +1,6 @@
 """Add voice columns for personalization
 
-Revision ID: 002
+Revision ID: 002_add_voice_cols
 Revises: 6c3bec1ee993
 Create Date: 2025-12-15 11:30:00.000000
 
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '002_add_voice_columns_for_personalization'
+revision: str = '002_add_voice_cols'
 down_revision: Union[str, None] = '6c3bec1ee993'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
