52f3ebe6ff25212ca7b31d9c68b51feb48da72f1
Dim41g <ivanov1331d@gmail.com>
2025-12-15 17:13:15 +0300

    Fix: Исправление идентификаторов миграций для устранения конфликта головных ревизий

--- alembic/versions/002_add_voice_columns_for_personalization.py
+++ alembic/versions/002_add_voice_columns_for_personalization.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '002'
+revision: str = '002_add_voice_columns_for_personalization'
 down_revision: Union[str, None] = '6c3bec1ee993'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
---
+++ alembic/versions/ea93055b8d1a_merge_migration_heads.py
@@ -0,0 +1,26 @@
+"""Merge migration heads
+
+Revision ID: ea93055b8d1a
+Revises: 001_add_content_types_data, 002_add_voice_columns_for_personalization
+Create Date: 2025-12-15 16:53:52.842205
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'ea93055b8d1a'
+down_revision: Union[str, None] = ('001_add_content_types_data', '002_add_voice_columns_for_personalization')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
